684ee1284d528c2e6d586555dce02025ff3b5f52
Marakanz <juwonakande07@gmail.com>
2022-05-08 14:12:06 -0500

    Connect github and heroku

--- models/Products.js
+++ models/Products.js
@@ -6,9 +6,10 @@ const productSchema = new mongoose.Schema({
     img: {type: String, required: true },
     categories: {type: Array, required: true},
     size: {type: Array },
+    color: {type: Array},
     price: {type: Number, required: true },
     inStock: {type: Boolean, default: true},
-    color: {type: String}
+    
 },
  {timestamps: true}
  );
